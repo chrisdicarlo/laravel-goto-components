c57367dad019424adec7d0d10ebac5c9b11c2e24
Chris Di Carlo <chris@dicarlosystems.ca>
2023-02-17 03:36:46 -0500

    Add basic Livewire component and Blade class-based component resolution

--- src/linkProvider.ts
+++ src/linkProvider.ts
@@ -1,63 +1,151 @@
 import { existsSync } from "fs";
 import {
-  DocumentLinkProvider,
-  Hover,
-  MarkdownString,
-  Position,
-  TextDocument,
-  Uri,
-  workspace,
-  ProviderResult,
-  DocumentLink,
-  Range,
+    DocumentLinkProvider,
+    Hover,
+    MarkdownString,
+    Position,
+    TextDocument,
+    Uri,
+    workspace,
+    ProviderResult,
+    DocumentLink,
+    Range,
 } from "vscode";
-import { nameToIndexPath, nameToPath } from "./utils";
+import { nameToIndexPath, nameToPath, livewireNameToIndexPath, livewireNameToPath, classNameToPath } from "./utils";
 
 export default class LinkProvider implements DocumentLinkProvider {
-  public provideDocumentLinks(
-    doc: TextDocument
-  ): ProviderResult<DocumentLink[]> {
-    const documentLinks: DocumentLink[] = [];
+    public provideDocumentLinks(
+        doc: TextDocument
+    ): ProviderResult<DocumentLink[]> {
+        const documentLinks: DocumentLink[] = [];
 
-    const config = workspace.getConfiguration("laravel_goto_components");
-    const workspacePath = workspace.getWorkspaceFolder(doc.uri)?.uri.fsPath;
+        const config = workspace.getConfiguration("laravel_goto_components");
+        const workspacePath = workspace.getWorkspaceFolder(doc.uri)?.uri.fsPath;
 
-    const reg = new RegExp(config.regex);
-    let linesCount = doc.lineCount;
+        const regBlade = new RegExp(config.regex.blade);
+        const regLivewireComponent = new RegExp(config.regex.livewireComponent);
+        const regLivewireDirective = new RegExp(config.regex.livewireDirective);
+        const regLivewire = new RegExp(config.regex.livewire);
+        let linesCount = doc.lineCount;
 
-    let index = 0;
-    while (index < linesCount) {
-      let line = doc.lineAt(index);
-      let result = line.text.match(reg);
+        let index = 0;
+        while (index < linesCount) {
+            let line = doc.lineAt(index);
+            let bladeResult = line.text.match(regBlade);
 
-      if (result !== null) {
-        for (let componentName of result) {
-          let componentPath = nameToPath(componentName);
-          
-          if (!existsSync(workspacePath + componentPath)) {
-            componentPath = nameToIndexPath(componentName);
-            
-            if (!existsSync(workspacePath + componentPath)) {
-              continue;
+            if (bladeResult !== null) {
+                for (let componentName of bladeResult) {
+                    let componentPath = nameToPath(componentName);
+
+                    if (!existsSync(workspacePath + componentPath)) {
+                        componentPath = nameToIndexPath(componentName);
+
+                        if (! existsSync(workspacePath + componentPath)) {
+                            componentPath = classNameToPath(componentName, doc.uri);
+
+                            if (!existsSync(workspacePath + componentPath)) {
+                                continue;
+                            }
+                        }
+                    }
+
+                    let start = new Position(
+                        line.lineNumber,
+                        line.text.indexOf(componentName)
+                    );
+                    let end = start.translate(0, componentName.length);
+                    let documentlink = new DocumentLink(
+                        new Range(start, end),
+                        Uri.file(workspacePath + componentPath)
+                    );
+                    documentLinks.push(documentlink);
+                }
             }
-          }
-          
-          let start = new Position(
-            line.lineNumber,
-            line.text.indexOf(componentName)
-          );
-          let end = start.translate(0, componentName.length);
-          let documentlink = new DocumentLink(
-            new Range(start, end),
-            Uri.file(workspacePath + componentPath)
-          );
-          documentLinks.push(documentlink);
+
+            let livewireResult = line.text.match(regLivewire);
+
+            if (livewireResult !== null) {
+                for (let componentName of livewireResult) {
+                    let componentPath = livewireNameToPath(componentName);
+
+                    if (!existsSync(workspacePath + componentPath)) {
+                        componentPath = livewireNameToIndexPath(componentName);
+
+                        if (componentPath == '' || !existsSync(workspacePath + componentPath)) {
+                            continue;
+                        }
+                    }
+
+                    let start = new Position(
+                        line.lineNumber,
+                        line.text.indexOf(componentName)
+                    );
+                    let end = start.translate(0, componentName.length);
+                    let documentlink = new DocumentLink(
+                        new Range(start, end),
+                        Uri.file(workspacePath + componentPath)
+                    );
+                    documentLinks.push(documentlink);
+                }
+            }
+
+            //   let livewireComponentResult = line.text.match(regLivewireComponent);
+
+            //   if (livewireComponentResult !== null) {
+            //     for (let componentName of livewireComponentResult) {
+            //       let componentPath = livewireNameToPath(componentName);
+
+            //        if (!existsSync(workspacePath + componentPath)) {
+            //          componentPath = livewireNameToIndexPath(componentName);
+
+            //         if (!existsSync(workspacePath + componentPath)) {
+            //           continue;
+            //         }
+            //        }
+
+            //       let start = new Position(
+            //         line.lineNumber,
+            //         line.text.indexOf(componentName)
+            //       );
+            //       let end = start.translate(0, componentName.length);
+            //       let documentlink = new DocumentLink(
+            //         new Range(start, end),
+            //         Uri.file(workspacePath + componentPath)
+            //       );
+            //       documentLinks.push(documentlink);
+            //     }
+            //   }
+
+            //   let livewireDirectiveResult = line.text.match(regLivewireDirective);
+
+            //   if (livewireDirectiveResult !== null) {
+            //     for (let componentName of livewireDirectiveResult) {
+            //       let componentPath = livewireNameToPath(componentName);
+
+            //        if (!existsSync(workspacePath + componentPath)) {
+            //          componentPath = livewireNameToIndexPath(componentName);
+
+            //         if (!existsSync(workspacePath + componentPath)) {
+            //           continue;
+            //         }
+            //        }
+
+            //       let start = new Position(
+            //         line.lineNumber,
+            //         line.text.indexOf(componentName)
+            //       );
+            //       let end = start.translate(0, componentName.length);
+            //       let documentlink = new DocumentLink(
+            //         new Range(start, end),
+            //         Uri.file(workspacePath + componentPath)
+            //       );
+            //       documentLinks.push(documentlink);
+            //     }
+            //   }
+
+            index++;
         }
-      }
 
-      index++;
+        return documentLinks;
     }
-
-    return documentLinks;
-  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,40 @@
+import { existsSync, readFile, readFileSync } from "fs";
+import { workspace, Uri } from "vscode";
+
 export function nameToPath(path: string): string {
-  return `/resources/views/components/${path.replace(/\./g, "/")}.blade.php`;
+    return `/resources/views/components/${path.replace(/\./g, "/")}.blade.php`;
 }
 
 export function nameToIndexPath(path: string): string {
-  return `/resources/views/components/${path.replace(/\./g, "/")}/index.blade.php`;
+    return `/resources/views/components/${path.replace(/\./g, "/")}/index.blade.php`;
+}
+
+export function livewireNameToPath(path: string): string {
+    return `/resources/views/livewire/${path.replace(/\./g, "/")}.blade.php`;
+}
+
+export function classNameToPath(path: string, uri: Uri): string {
+    const workspacePath = workspace.getWorkspaceFolder(uri)?.uri.fsPath;
+
+    const parts = path.replace(/\./g, "/").split('-');
+    const className = parts.map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('');
+    const classPath = `/app/View/Components/${className}.php`;
+
+    if (existsSync(workspacePath + classPath)) {
+        let file = readFileSync(workspacePath + classPath, 'utf-8');
+
+        if (file) {
+            let view = file.match(/(?<=return view\([\\'\"])[a-z.-]+/g);
+
+            if (view) {
+                return `/resources/views/${view[0].replace(/\./g, "/")}.blade.php`;
+            }
+        }
+    }
+
+    return '';
+}
+
+export function livewireNameToIndexPath(path: string): string {
+    return `/resources/views/livewire/${path.replace(/\./g, "/")}/index.blade.php`;
 }
